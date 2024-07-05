e792a3318f00e870fd3b004725f7291984f847af
Jake Hamblin <jake@jakehamblin.com>
2024-07-05 00:02:37 -0400

    Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# StratisLand
+A simple PHP landing page to be used by individuals and businesses. Provides a number of customizable options for end users, including custom text, colors, and images.
+
+## Installation
+**PHP 8.1 or higher is required**
+
+To install StratisLand, you first need to being by acquiring a domain and hosting. For both, I recommend [Hosturly](https://hambl.in/hosturly). I am a partner of Hosturly and longtime user. I currently own my own dedicated server and colocate it through Hosturly and I absolutely love their service. For specific steps on setting up shared hosting with Hosturly, take a look at my [documentation](https://docs.jakehamblin.com/shared-hosting) on shared hosting.
+
+After acquiring hosting and a domain, you need to upload this repository into the root folder for the domain. This can be done by downloading this repository or using cPanel's git client. With this now completed, you can rename the `config.php.template` folder to `config.php` and begin filling out the config. After filling out the config, you are ready to go
+
+## Hosting Company
+Are you looking for a reliable and affordable host? Well, with many years of experience working with websites, website hosting, and dedicated server hosting, I can say that I've got a pretty good idea when a host is good. [Hosturly](https://hambl.in/hosturly) is up for the task. Don't trust me? Read their reviews. They've got an almost 5 star rating, and with many years in the industry, they're here to stay. Use code `JAKE` for 10% off your order.
+<br>
+<br>
+<p align="center">
+ <a href="https://hambl.in/hosturly"><img src="https://jakehamblin.com/images/hosturly.png"></a>
+</p>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+    /**
+     * File: index.php
+     * Author: Jake Hamblin
+     * Date: July 4, 2024
+     * Description: A PHP site using HTML and CSS that provides a simple landing page with customizable options
+    */
+
     require("config.php");
 ?><!DOCTYPE html>
 <html>
@@ -9,25 +16,25 @@
     <!-- Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#<?php echo INFORMATION['meta']['color'] ?>">
-	<meta name="twitter:card" content="summary">
-	<meta name="twitter:creator" content="@jekeltor">
-	<meta property="og:url" content="<?php echo INFORMATION['meta']['domain'] ?>">
-	<meta property="og:title" content="<?php echo ((is_null(INFORMATION['meta']['titleTag'])) ? "" : INFORMATION['meta']['titleTag']." - ").INFORMATION['meta']['name'] ?>">
-	<meta property="og:description" content="<?php echo INFORMATION['meta']['description'] ?>">
-	<meta property="og:image" content="<?php echo INFORMATION['meta']['logo'] ?>">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:creator" content="@jekeltor">
+    <meta property="og:url" content="<?php echo INFORMATION['meta']['domain'] ?>">
+    <meta property="og:title" content="<?php echo ((is_null(INFORMATION['meta']['titleTag'])) ? "" : INFORMATION['meta']['titleTag']." - ").INFORMATION['meta']['name'] ?>">
+    <meta property="og:description" content="<?php echo INFORMATION['meta']['description'] ?>">
+    <meta property="og:image" content="<?php echo INFORMATION['meta']['logo'] ?>">
 
     <!-- Stylesheets -->
-    <!--<link rel="stylesheet" href="css/login.css">-->
+    <link rel="stylesheet" href="./css/main.css?rnd=1.0">
 
     <!-- Links -->
-	<link rel="icon" type="image/png" href="<?php echo INFORMATION['meta']['logo'] ?>">
+    <link rel="icon" type="image/png" href="<?php echo INFORMATION['meta']['logo'] ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 
 
     <!-- Scripts -->
-	<script src="https://kit.fontawesome.com/<?php echo $information['fontAwesomeKit'] ?>.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/<?php echo INFORMATION['fontAwesomeKit'] ?>.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <style>
@@ -38,66 +45,30 @@
             --foreground-color: #<?php echo INFORMATION['colors']['foreground'] ?>;
             --preloader-color: #<?php echo INFORMATION['colors']['preloader'] ?>;
         }
-
-        body {
-            font-family: "Open Sans", sans-serif;
-            display: flex;
-            min-height: 100vh;
-            width: 100vw;
-            margin: 0;
-            background-color: var(--background-color);
-            align-items: center;
-            justify-content: center;
-            text-align: center;
-            color: var(--text-color);
-        }
-
-        .preloader {
-            position: absolute;
-            top: 0;
-            left: 0;
-            visibility: visible;
-            opacity: 1;
-            height: 100%;
-            width: 100%;
-            background-color: var(--preloader-color);
-            transition: opacity .75s ease, visibility .75s ease;
-        }
-
-        .preloader.loaded {
-            visibility: hidden;
-            opacity: 0;
-        }
-
-        .author p {
-            margin: 0;
-        }
-
-        .author img.logo {
-            height: 40vh;
-            width: auto;
-        }
-
-        .author p.name {
-            font-weight: bold;
-            font-size: 3.5vh;
-            margin-bottom: 1.5vh;
-        }
     </style>
 </head>
 <body>
     <section class="preloader"></section>
     <section class="author">
-        <?php echo !is_null(INFORMATION['author']['logo']['link']) ? "<img class='logo' src='".INFORMATION['author']['logo']['link']."' alt='AUTHOR_LOGO'>" : "" ?>
+        <?php echo !is_null(INFORMATION['author']['logo']['link']) ? "<img class='logo".(INFORMATION['author']['logo']['round'] == TRUE ? " round" : "")."' src='".INFORMATION['author']['logo']['link']."' alt='AUTHOR_LOGO'>" : "" ?>
         <p class="name"><?php echo INFORMATION['author']['name'] ?></p>
-        <p class="description"><?php echo INFORMATION['author']['description'] ?></p>
+        <?php echo !is_null(INFORMATION['author']['description']) ? "<p class='description'>".INFORMATION['author']['description']."</p>" : "" ?>
+        <?php
+            if(count(INFORMATION['media']) > 0) {
+        ?>
         <div class="media">
+            <?php
+                foreach(INFORMATION['media'] as $media) {
+            ?>
+            <a class="social" href="<?php echo $media['link'] ?>" target="_blank"><i class="<?php echo $media['tag'] ?>"></i></a> 
+            <?php
+                }
+            ?>
         </div>
+        <?php
+            }
+        ?>
     </section>
-    <script>
-        window.onload = function() {
-            document.querySelector(".preloader").classList.add("loaded");
-        }
-    </script>
+    <script src="./js/onload.js"></script>
 </body>
 </html>
